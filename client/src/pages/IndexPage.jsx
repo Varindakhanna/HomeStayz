6117ea1df8b5908530f857a787d9793e966844f4
VARINDA KHANNA <varindakhanna2002@gmail.com>
2023-04-25 14:17:04 +0530

    first commit

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,16 @@
+
+import Header from '../Header' 
+
+function IndexPage()
+{
+    return(
+        <div>
+         INDEX PG HERE right
+         hello 
+      </div>
+    );
+    
+        
+}
+
+export default IndexPage;
